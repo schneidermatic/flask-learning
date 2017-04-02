a38e97703f9760851117ef72cac351c5d4f3c6e5
schneidermatic <markus.schneider73@gmail.com>
2017-04-02 09:48:15 +0200

    Initial commit of 'chap5.1'.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	new file:   chap5.1/README.txt
    	new file:   chap5.1/activate
    	new file:   chap5.1/app/__init__.py
    	new file:   chap5.1/app/app.py
    	new file:   chap5.1/app/config.py
    	new file:   chap5.1/app/entries/__init__.py
    	new file:   chap5.1/app/entries/blueprint.py
    	new file:   chap5.1/app/entries/forms.py
    	new file:   chap5.1/app/entries/templates/entries/base_entries.html
    	new file:   chap5.1/app/entries/templates/entries/create.html
    	new file:   chap5.1/app/entries/templates/entries/delete.html
    	new file:   chap5.1/app/entries/templates/entries/detail.html
    	new file:   chap5.1/app/entries/templates/entries/edit.html
    	new file:   chap5.1/app/entries/templates/entries/image_upload.html
    	new file:   chap5.1/app/entries/templates/entries/index.html
    	new file:   chap5.1/app/entries/templates/entries/tag_detail.html
    	new file:   chap5.1/app/entries/templates/entries/tag_index.html
    	new file:   chap5.1/app/entries/templates/includes/list.html
    	new file:   chap5.1/app/entries/templates/macros/form_field.html
    	new file:   chap5.1/app/forms.py
    	new file:   chap5.1/app/helpers.py
    	new file:   chap5.1/app/main.py
    	new file:   chap5.1/app/manage.py
    	new file:   chap5.1/app/models.py
    	new file:   chap5.1/app/scripts/create_db.py
    	new file:   chap5.1/app/static/css/bootstrap.min.css
    	new file:   chap5.1/app/static/fonts/glyphicons-halflings-regular.eot
    	new file:   chap5.1/app/static/fonts/glyphicons-halflings-regular.svg
    	new file:   chap5.1/app/static/fonts/glyphicons-halflings-regular.ttf
    	new file:   chap5.1/app/static/fonts/glyphicons-halflings-regular.woff
    	new file:   chap5.1/app/static/fonts/glyphicons-halflings-regular.woff2
    	new file:   chap5.1/app/static/js/bootstrap.min.js
    	new file:   chap5.1/app/static/js/jquery-1.10.2.min.js
    	new file:   chap5.1/app/sys
    	new file:   chap5.1/app/templates/homepage.html
    	new file:   chap5.1/app/templates/includes/page_links.html
    	new file:   chap5.1/app/templates/layouts/base.html
    	new file:   chap5.1/app/templates/login.html
    	new file:   chap5.1/app/templates/macros/form_field.html
    	new file:   chap5.1/app/views.py
    	new file:   chap5.1/deactivate
    	new file:   chap5.1/requirements.txt
    	new file:   chap5.1/run.sh
    	new file:   chap5.1/setup

---
+++ chap5.1/app/entries/blueprint.py
@@ -0,0 +1,125 @@
+import os
+from flask import Blueprint
+from flask import flash
+from flask import render_template
+from flask import request
+from helpers import object_list
+from models import Entry, Tag
+from app import app
+from app import db
+from entries.forms import EntryForm
+from entries.forms import ImageForm
+from flask import redirect
+from flask import url_for
+from sqlalchemy.orm import sessionmaker
+from werkzeug import secure_filename
+
+entries = Blueprint('entries', __name__, template_folder='templates')
+
+def entry_list(template, query, **context):
+    app.logger.info("---> Hello")
+    valid_statuses = (Entry.STATUS_PUBLIC, Entry.STATUS_DRAFT)
+    query = query.filter(Entry.status.in_(valid_statuses))
+
+    if request.args.get('q'):
+        search = request.args['q']
+        query = query.filter(Entry.body.contains(search) |
+                            (Entry.title.contains(search)))
+
+    return object_list(template, query, **context)
+
+
+def get_entry_or_404(slug):
+    #valid_statuses = (Entry.STATUS_PUBLIC, Entry.STATUS_DRAFT)
+    valid_statuses = (Entry.STATUS_PUBLIC, Entry.STATUS_DRAFT)
+    entry = Entry.query.filter((Entry.slug == slug) & (Entry.status.in_(valid_statuses))).first_or_404()
+    return entry
+
+@entries.route('/')
+def index():
+    entries = Entry.query.order_by(Entry.created_timestamp.desc())
+    #return object_list('entries/index.html', entries)
+    return entry_list('entries/index.html', entries)
+
+@entries.route('/tags/')
+def tag_index():
+    tags = Tag.query.order_by(Tag.name)
+    return object_list('entries/tag_index.html', tags)
+
+@entries.route('/image-upload/', methods=['GET','POST'])
+def image_upload():
+    if request.method == 'POST':
+        form = ImageForm(request.form)
+        if form.validate():
+            image_file = request.files['file']
+            filename = os.path.join(app.config['IMAGES_DIR'], secure_filename(image_file.filename))
+            image_file.save(filename)
+            flash('Saved %s' % os.path.basename(filename),'success')
+            return redirect(url_for('entries.index'))
+    else:
+        form = ImageForm()
+
+    return render_template('entries/image_upload.html', form=form)
+
+@entries.route('/tags/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug == slug).first_or_404()
+    entries = tag.entries.order_by(Entry.created_timestamp.desc())
+    return object_list('entries/tag_detail.html', entries, tag=tag)
+
+@entries.route('/<slug>/')
+def detail(slug):
+    #entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    entry = get_entry_or_404(slug)
+    return render_template('entries/detail.html', entry=entry)
+
+@entries.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        form = EntryForm(request.form)
+        if form.validate():
+            entry = form.save_entry(Entry())
+            for tag in entry.tags:
+                app.logger.info("***> " + str(tag.id) + "|" + str(tag.name))
+            db.session.add(entry)
+            db.session.commit()
+            flash('Entry "%s" created successfully.' % entry.title, 'success')
+            return redirect(url_for('entries.detail', slug=entry.slug))
+    else:
+        form = EntryForm()
+
+    return render_template('entries/create.html', form=form)
+
+@entries.route('/<slug>/edit', methods=['GET', 'POST'])
+def edit(slug):
+    #entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    entry = get_entry_or_404(slug)
+    if request.method == 'POST':
+        app.logger.info("===> edit")
+        form = EntryForm(request.form, obj=entry)
+        if form.validate():
+            entry = form.save_entry(entry)
+            for tag in entry.tags:
+                app.logger.info("***> " + str(tag.id) + "|" + str(tag.name))
+            db.session.add(entry)
+            db.session.commit()
+            flash('Entry "%s" has been saved.' % entry.title, 'success')
+            return redirect(url_for('entries.detail',slug=entry.slug))
+    else:
+        form = EntryForm(obj=entry)
+
+    return render_template('entries/edit.html', entry=entry, form=form)
+
+
+@entries.route('/<slug>/delete/', methods=['GET', 'POST'])
+def delete(slug):
+    #entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    entry = get_entry_or_404(slug)
+    if request.method == 'POST':
+        entry.status = Entry.STATUS_DELETED
+        db.session.add(entry)
+        db.session.commit()
+        flash('Entry "%s" has been deleted.' % entry.title, 'success')
+        return redirect(url_for('entries.index'))
+
+    return render_template('entries/delete.html', entry=entry)
